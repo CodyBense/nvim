ed2d71fa23765a032e90739f45ecd8af64731750
CodyBense <codybense@gmail.com>
2024-02-09 20:49:06 -0500

    removed live-server keybinds

--- lua/cody/plugins/live-server.lua
+++ lua/cody/plugins/live-server.lua
@@ -2,8 +2,5 @@ return{
     'barrett-ruth/live-server.nvim',
     build = 'pnpm add -g live-server',
     cmd = { 'LiveServerStart', 'LiveServerStop' },
-    config = function()
-        vim.keymap.set('n', '<leader>l', vim.cmd.LiveServerStart)
-        vim.keymap.set('n', '<leader>ls', vim.cmd.LiveServerStop)
-    end
+    config = true
 }
