6d4e51746154a85c98a7b4eca3061115c045b370
CodyBense <codybense@gmail.com>
2024-06-26 16:31:52 -0400

    plenary stuff

--- lua/cody/plugins/init.lua
+++ lua/cody/plugins/init.lua
@@ -1,11 +1,12 @@
 -- for plugins with no extra config
 return {
 
-    {
-        "nvim-lua/plenary.nvim",
-        name = "plenary"
-    },
+    -- {
+    --     "nvim-lua/plenary.nvim",
+    --     name = "plenary"
+    -- },
 
+    'nvim-lua/plenary.nvim',
     'nvim-treesitter/playground',
     'folke/lsp-colors.nvim',
 }
