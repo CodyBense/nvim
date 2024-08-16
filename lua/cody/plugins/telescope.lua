faf334c92df236f6054f0e3ba12e13fe7db0d799
CodyBense <codybense@gmail.com>
2024-08-16 09:49:58 -0400

    fixing

--- lua/cody/plugins/telescope.lua
+++ lua/cody/plugins/telescope.lua
@@ -5,17 +5,14 @@ return {
 
     dependencies = {
         'nvim-lua/plenary.nvim',
-        'BurntSushi/ripgrep'
+        'BurntSushi/ripgrep',
     },
 
-            builtin.live_grep({
     config = function()
-            builtin.live_grep({
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n' , '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
