a7f46b345a45ce11d95e7d8587bdcce8caccab90
CodyBense <codybense@gmail.com>
2025-10-26 08:31:04 -0400

    UPDATE: harpoon

--- lua/cody/plugins/harpoon.lua
+++ lua/cody/plugins/harpoon.lua
@@ -11,11 +11,10 @@ return {
     keys = {
         { "<leader>a", function() require("harpoon"):list():add() end, desc = "Mark file"},
         { "<C-e>", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Toggle quick menu"},
-        { "<C-1>", function() require("harpoon"):list():select(1) end, desc = "Switch to first file"},
-        { "<C-2>", function() require("harpoon"):list():select(2) end, desc = "Switch to second file"},
-        { "<C-3>", function() require("harpoon"):list():select(3) end, desc = "Switch to third file"},
-        { "<C-4>", function() require("harpoon"):list():select(4) end, desc = "Switch to fourth file"},
-        { "<C-5>", function() require("harpoon"):list():select(5) end, desc = "Switch to fourth file"},
+        { "<C-h>", function() require("harpoon"):list():select(1) end, desc = "Switch to first file"},
+        { "<C-j>", function() require("harpoon"):list():select(2) end, desc = "Switch to second file"},
+        { "<C-k>", function() require("harpoon"):list():select(3) end, desc = "Switch to third file"},
+        { "<C-l>", function() require("harpoon"):list():select(4) end, desc = "Switch to fourth file"},
         { "mr", function() require("harpoon"):list():remove() end, desc = "Remove location"},
     }
 }
